df5254da859624c0068331604b221efec28479b1
Shoaib Wani <wanishoaib@protonmail.com>
2025-10-10 17:33:51 +0530

    fix

--- src/routes/transformed_request.routes.js
+++ src/routes/transformed_request.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { TransformedRequestsController } from "../controllers/transformed-requests.controller" 
+import { TransformedRequestsController } from "../controllers/transformed-requests.controller.js" 
 
 const router = express.Router();
 
