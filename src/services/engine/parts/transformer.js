896798e5e5c15b2d07cf91320b7866a4fd9e7bc0
Alpha <syedaarishshah@gmail.com>
2025-10-14 15:52:49 +0530

    undo transformer.js

--- src/services/engine/parts/transformer.js
+++ src/services/engine/parts/transformer.js
@@ -25,14 +25,8 @@ const headerObjectToArray = (headersObj) => {
 export const transform = ({ request, rule }) => {
   const original = { ...request };
 
+
   const baseUrl = new URL(original.url);
-  
-  // Append params from the request object to the URL's search params
-  if (original.params) {
-    for (const [key, value] of Object.entries(original.params)) {
-      baseUrl.searchParams.set(key, String(value)); // Ensure value is a string
-    }
-  }
 
   // === 1. Handle Header Transformations ===
   // let headers = arrayToHeaderObject(original.headers || []);
@@ -111,7 +105,6 @@ export const transform = ({ request, rule }) => {
     headers: headerObjectToArray(headers),
     url: baseUrl.toString(),
   };
-  delete transformedRequest.params;
 
   // === 7. Repeat request with multiple methods ===
   const methodVariants = rule.transform?.repeat_with_methods || [transformedRequest.method];
