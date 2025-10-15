7232a1724e234bde5231dcabf4fac7d3e803d844
Shoaib Wani <wanishoaib@protonmail.com>
2025-10-15 12:58:37 +0530

    fix: fixed refresh as well

--- src/services/integration.service.js
+++ src/services/integration.service.js
@@ -200,7 +200,9 @@ class IntegrationService {
             await integration.save();
 
             // Sync again
-            await this.syncIntegration(integration, apiKey);
+            // await this.syncIntegration(integration, apiKey);
+            // await this.syncIntegration(integration, apiKey, environment);
+            await mqbroker.publish("apisec", "apisec.integration.sync", { integration, apiKey, environment });
 
             // Return updated integration without sensitive data
             const updatedIntegration = await Integration.findById(id).select('-apiKey').lean();
