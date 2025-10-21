61c60ed7d7998ff56f3ce8e06fd6442f11f27d6b
Alpha <syedaarishshah@gmail.com>
2025-10-21 17:46:29 +0530

    add log and remove strict

--- src/workers/scan.worker.js
+++ src/workers/scan.worker.js
@@ -473,8 +473,10 @@ async function runAndMatchRequests(payload, msg, channel) {
                 }
             };
 
+            console.log("[+] Vulnerability data to be saved:", vulnerabilityData);
+
             try {
-                await Vulnerability.create([vulnerabilityData], { strict: false });
+                await Vulnerability.create([vulnerabilityData]);
                 console.log(`[+] Created vulnerability record - ${vulnerabilityData.title}`);
             } catch (vulnError) {
                 console.error("[!] Error creating vulnerabilities:", vulnError);
