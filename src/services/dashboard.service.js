7f406d618ce33e5baeb92bf7d9c5267232fec093
Alpha <syedaarishshah@gmail.com>
2025-10-21 16:20:39 +0530

    test

--- src/services/dashboard.service.js
+++ src/services/dashboard.service.js
@@ -150,7 +150,7 @@ class DashboardService {
         .limit(5)
         .populate('ruleId', 'ruleName category')
         .populate('requestId', 'name url method collectionName')
-        .populate('transformedRequestId', 'name url method collectionName')
+        // .populate('transformedRequestId', 'name url method collectionName')
         .lean();
 
         // Map severity to numeric value for proper sorting
