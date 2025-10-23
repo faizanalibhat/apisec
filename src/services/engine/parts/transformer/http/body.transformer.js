8e412fafe5ed53624adecb96f7e2d61cfb8d0cff
Shoaib Wani <wanishoaib@protonmail.com>
2025-10-23 21:40:20 +0530

    fixed

--- src/services/engine/parts/transformer/http/body.transformer.js
+++ src/services/engine/parts/transformer/http/body.transformer.js
@@ -1,171 +1,110 @@
 import _ from 'lodash';
 
+
+
+// helper functions one for each operation
+function add(body, newParams) {
+  Object.assign(body, newParams);
+}
+
+function remove(body, removeParams) {
+  removeParams.forEach(param => delete body[param]);
+}
+
+function modify(body, modifyParams) {
+  Object.entries(modifyParams).forEach(([key, value]) => {
+    if (body[key]) body[key] = value;
+  });
+}
+
+function replace_all_values(body, value) {
+  Object.keys(body).forEach(key => {
+    body[key] = value;
+  });
+}
+
+
+function replace_all_values_one_by_one(body, value) {
+  const paramKeys = Object.keys(body);
+
+  const clonedList = [];
+
+  for (let key of paramKeys) {
+    const clonedParams = _.cloneDeep(body);
+    clonedParams[key] = value;
+
+    clonedList.push(clonedParams);
+  }
+
+  return clonedList;
+}
+
+
+function handleTransformation(body, transformations) {
+  const allParams = [];
+
+  for (let transformation of transformations) {
+    const cloneParam = _.cloneDeep(body);
+    allParams.push(...(applyRules(cloneParam, transformation) || []));
+  }
+
+  return allParams;
+}
+
+
+
+function applyRules(body, rules) {
+  let allParams = [];
+
+  if (rules.transformations) {
+    allParams = handleTransformation(body, rules.transformations);
+
+    return allParams;
+  }
+
+  if (rules.add) {
+    add(body, rules.add);
+  }
+
+  if (rules.remove) {
+    remove(body, rules.remove);
+  }
+
+  if (rules.modify) {
+    modify(body, rules.modify);
+  }
+
+  if (rules.replace_all_values) {
+    replace_all_values(body, rules.replace_all_values);
+  }
+
+  if (rules.replace_all_values_one_by_one) {
+    allParams = replace_all_values_one_by_one(body, rules.replace_all_values_one_by_one);
+  }
+
+  return allParams.length > 0 ? allParams : [params];
+}
+
+
+
+
 export default {
   transform(request, bodyRules) {
+
     if (!bodyRules) return [request];
 
+    let requests = [_.cloneDeep(request)];
 
-    let requests = [];
+    const targetBody = requests[0].body || {};
 
-    // Handle transformations array (creates separate requests for each transformation)
-    if (Array.isArray(bodyRules.transformations)) {
-      for (const transformation of bodyRules.transformations) {
-        const req = _.cloneDeep(request);
-        const body = req.body || {};
-        this._applyTransformation(body, transformation);
-        req.body = body;
-        requests.push(req);
-      }
-    }
+    const transformedParams = applyRules(targetBody, bodyRules);
 
-    // If no transformation rules were applied, start with the original
-    if (requests.length === 0) requests = [_.cloneDeep(request)];
-
-    // Apply global rules (if any)
-    if (bodyRules.add || bodyRules.remove || bodyRules.modify ||
-        bodyRules.replace_all || bodyRules.replace_all_one_by_one) {
-      const globalReqs = requests.flatMap(req => this._applyGlobalRules(req, bodyRules));
-      if (globalReqs.length > 0) {
-        requests = globalReqs;
-      }
-    }
-
-    // ✅ Always return at least one request
-    return requests.length > 0 ? requests : [_.cloneDeep(request)];
-  },
-
-  _applyTransformation(body, transformation) {
-    Object.entries(transformation).forEach(([operator, config]) => {
-      if (operator === 'add' && typeof config === 'object') {
-        Object.assign(body, config);
-      } else if (operator === 'remove' && Array.isArray(config)) {
-        config.forEach(field => delete body[field]);
-      } else if (operator === 'modify' && typeof config === 'object') {
-        Object.entries(config).forEach(([field, cfg]) => {
-          body[field] = this._applyModification(body[field], cfg);
-        });
-      }
+    requests = transformedParams.map(body => {
+      const newRequest = _.cloneDeep(request);
+      newRequest.body = body;
+      return newRequest;
     });
-  },
 
-  _applyGlobalRules(request, bodyRules) {
-    const requests = [_.cloneDeep(request)];
-    const body = requests[0].body || {};
-
-    // Add fields
-    if (bodyRules.add) Object.assign(body, bodyRules.add);
-
-    // Remove fields
-    if (Array.isArray(bodyRules.remove)) {
-      bodyRules.remove.forEach(field => delete body[field]);
-    }
-
-    // Modify fields
-    if (bodyRules.modify) {
-      Object.entries(bodyRules.modify).forEach(([field, config]) => {
-        body[field] = this._applyModification(body[field], config);
-      });
-    }
-
-    // Replace all values with a string
-    if (bodyRules.replace_all) {
-      this._replaceAllValues(body, bodyRules.replace_all);
-    }
-
-    // Replace all values one by one (create separate requests)
-    if (bodyRules.replace_all_one_by_one) {
-      const replaced = this._replaceAllOneByOne(request, body, bodyRules.replace_all_one_by_one);
-      // ✅ ensure we never return empty
-      return replaced.length > 0 ? replaced : [_.cloneDeep(request)];
-    }
-
-    requests[0].body = body;
     return requests;
-  },
-
-  _applyModification(currentValue, config) {
-    if (typeof config === 'string') {
-      return config;
-    } else if (typeof config === 'object') {
-      let newValue = config.value !== undefined ? config.value : currentValue;
-      if (config.prefix) newValue = config.prefix + newValue;
-      if (config.suffix) newValue = newValue + config.suffix;
-      return newValue;
-    }
-    return currentValue;
-  },
-
-  _replaceAllValues(obj, replacement) {
-    Object.keys(obj).forEach(key => {
-      if (typeof obj[key] === 'string' || typeof obj[key] === 'number') {
-        obj[key] = replacement;
-      } else if (typeof obj[key] === 'object' && obj[key] !== null) {
-        this._replaceAllValues(obj[key], replacement);
-      }
-    });
-  },
-
-  _replaceAllOneByOne(originalRequest, body, replacement) {
-    const values = [];
-
-    if (!body) return [_.cloneDeep(originalRequest)]
-
-    // if body is json parsable
-    let json = null;
-
-    console.log(body);
-
-    try {
-      let body_payload = body;
-
-      body_payload = body_payload?.replace?.(/[\s\n\t]+/g, '');
-
-      json = JSON.parse(body_payload);
-    }
-    catch(err) {
-      // console.log(err.message);
-      // console.log("not json body : ", body);
-      return [_.cloneDeep(originalRequest)]
-    }
-
-    if (!json) {
-        return [_.cloneDeep(originalRequest)]
-    }
-
-    this._collectAllValues(json, values);
-
-    // ✅ if no values to replace, return original
-    if (values.length === 0) return [_.cloneDeep(originalRequest)];
-
-    return values.map((x, index) => {
-      const req = _.cloneDeep(originalRequest);
-      const newBody = _.cloneDeep(json);
-      this._replaceValueAtIndex(newBody, replacement, index, { count: 0 });
-      req.body = newBody;
-      return req;
-    });
-  },
-
-  _collectAllValues(obj, values) {
-    Object.values(obj).forEach(val => {
-      if (typeof val === 'string' || typeof val === 'number') {
-        values.push(val);
-      } else if (typeof val === 'object' && val !== null) {
-        this._collectAllValues(val, values);
-      }
-    });
-  },
-
-  _replaceValueAtIndex(obj, replacement, targetIndex, counter) {
-    Object.keys(obj).forEach(key => {
-      if (typeof obj[key] === 'string' || typeof obj[key] === 'number') {
-        if (counter.count === targetIndex) {
-          obj[key] = replacement;
-        }
-        counter.count++;
-      } else if (typeof obj[key] === 'object' && obj[key] !== null) {
-        this._replaceValueAtIndex(obj[key], replacement, targetIndex, counter);
-      }
-    });
   }
 };
