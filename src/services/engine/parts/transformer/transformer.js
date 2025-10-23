f1c6399d068f0d77e9ec533a651f38e441a165c6
Shoaib Wani <wanishoaib@protonmail.com>
2025-10-23 20:19:58 +0530

    fixed issues

--- src/services/engine/parts/transformer/transformer.js
+++ src/services/engine/parts/transformer/transformer.js
@@ -28,23 +28,21 @@ export const transformer = {
 
 
     // Apply component transformers
-    if (transformRules.path) {
-      requests = requests.flatMap(req => pathTransformer.transform(req, transformRules.path));
-    }
-
+    // if (transformRules.path) {
+    //   requests = requests.flatMap(req => pathTransformer.transform(req, transformRules.path));
+    // }
 
     if (transformRules.query) {
       requests = requests.flatMap(req => queryTransformer.transform(req, transformRules.query));
-      console.log("[+] appliying rules: ", transformRules.query, requests);
     }
 
-    if (transformRules.header) {
-      requests = requests.flatMap(req => headersTransformer.transform(req, transformRules.header));
-    }
+    // if (transformRules.header) {
+    //   requests = requests.flatMap(req => headersTransformer.transform(req, transformRules.header));
+    // }
 
-    if (transformRules.body) {
-      requests = requests.flatMap(req => bodyTransformer.transform(req, transformRules.body));
-    }
+    // if (transformRules.body) {
+    //   requests = requests.flatMap(req => bodyTransformer.transform(req, transformRules.body));
+    // }
 
     requests = requests.map(req => this._rebuildUrl(req));
 
@@ -77,11 +75,13 @@ export const transformer = {
   },
 
   _applyMethod(requests, methods) {
+
     if (!Array.isArray(methods)) {
       methods = [methods];
     }
 
     const result = [];
+
     requests.forEach(req => {
       methods.forEach(method => {
         const newReq = _.cloneDeep(req);
