16e45d07b7a1d94c820b53c93eda5212c1b66746
Alpha <syedaarishshah@gmail.com>
2025-11-04 14:39:58 +0530

    Fix transformer to return an empty array when no transform rules are present insteads of raw req

--- src/services/engine/parts/transformer/transformer.js
+++ src/services/engine/parts/transformer/transformer.js
@@ -10,7 +10,7 @@ export const transformer = {
   transform({ request, rule, authProfile }) {
     const transformRules = rule.transform;
 
-    if (!transformRules) return [request];
+    if (!transformRules) return [];
 
     let requests = [_.cloneDeep(request)];
 
