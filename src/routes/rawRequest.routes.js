bfb026512299b7e6ab5e767a882c25f5411d6aa3
Alpha <syedaarishshah@gmail.com>
2025-11-10 17:03:13 +0530

    remove validation

--- src/routes/rawRequest.routes.js
+++ src/routes/rawRequest.routes.js
@@ -8,13 +8,35 @@ const router = express.Router();
 router.post('/bulk/delete', validation.validateBulkDelete, rawRequestController.bulkDelete);
 
 router
-  // .post('/', validation.validateCreateRawRequest, rawRequestController.createRawRequest)
   .post('/', rawRequestController.createRawRequest)
-  // .get('/', validation.validateGetRawRequests, rawRequestController.getRawRequests)
   .get('/', rawRequestController.getRawRequests)
+  .get('/:id', rawRequestController.getRawRequest)
+  .put('/:id', rawRequestController.updateRawRequest)
+  .delete('/:id', rawRequestController.deleteRawRequest);
+
+
+export default router;
+
+
+/* Validation
+
+import express from 'express';
+import * as rawRequestController from '../controllers/rawRequest.controller.js';
+import * as validation from '../middleware/validation/rawRequest.validation.js';
+
+const router = express.Router();
+
+// Bulk operations
+router.post('/bulk/delete', validation.validateBulkDelete, rawRequestController.bulkDelete);
+
+router
+  .post('/', validation.validateCreateRawRequest, rawRequestController.createRawRequest)
+  .get('/', validation.validateGetRawRequests, rawRequestController.getRawRequests)
   .get('/:id', validation.validateObjectId, rawRequestController.getRawRequest)
   .put('/:id', validation.validateObjectId, rawRequestController.updateRawRequest)
   .delete('/:id', validation.validateObjectId, rawRequestController.deleteRawRequest);
 
 
 export default router;
+
+*/
