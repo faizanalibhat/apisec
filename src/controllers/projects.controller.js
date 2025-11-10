8a8fb98f38e1cb216665c23d957e3f09859933cc
Alpha <syedaarishshah@gmail.com>
2025-11-10 17:47:35 +0530

    test

--- src/controllers/projects.controller.js
+++ src/controllers/projects.controller.js
@@ -381,7 +381,7 @@ class ProjectsController {
                 orgId,
                 method: rawRequestData.method,
                 url: rawRequestData.url,
-                projectId: projectId,
+                projectIds: projectId,
                 source: 'browser-extension'
             });
 
