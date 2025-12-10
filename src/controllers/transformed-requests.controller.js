d4979c32b96a00279fa069df8eef928ae7fd4fdf
Shoaib Wani <wanishoaib@protonmail.com>
2025-12-10 12:49:37 +0530

    autocommit

--- src/controllers/transformed-requests.controller.js
+++ src/controllers/transformed-requests.controller.js
@@ -41,7 +41,7 @@ export class TransformedRequestsController {
             filters.vulnerabilityDetected = hasVulns === 'true';
         }
 
-        if (projectId) filters.projectId = ObjectId.createFromHexString(projectId);
+        if (projectId) filters.projectId = { $in: [ObjectId.createFromHexString(projectId)] };
 
         console.log("JSON FILTERS: ", JSON.stringify(filters));
 
