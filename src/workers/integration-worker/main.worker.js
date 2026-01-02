cfcaeb9cf48bbff27de3f1343a3e68420d525c51
Shoaib Wani <wanishoaib@protonmail.com>
2026-01-02 06:02:33 +0530

    added refresh too

--- src/workers/integration-worker/main.worker.js
+++ src/workers/integration-worker/main.worker.js
@@ -4,10 +4,11 @@ import { APPLICATION_EXCHANGE_NAME ,INTEGRATION_EVENT_ROUTING_KEYS } from "../..
 
 // event handlers
 import { handleInstallIntegration } from "./handlers/install.handler.js";
-
+import { handleRefreshIntegration } from "./handlers/refresh.handler.js";
 
 
 export async function integrationEventsWorker() {
 
     await mqbroker.consume(APPLICATION_EXCHANGE_NAME, INTEGRATION_EVENT_ROUTING_KEYS.INSTALL_INTEGRATION, handleInstallIntegration, "install_integration_events_queue");
+    await mqbroker.consume(APPLICATION_EXCHANGE_NAME, INTEGRATION_EVENT_ROUTING_KEYS.REFRESH_INTEGRATION, handleRefreshIntegration, "refresh_integration_events_queue");
 }
