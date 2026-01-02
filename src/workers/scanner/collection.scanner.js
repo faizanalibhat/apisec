024b0c78da042abcdefa46b492337e88d15ce02e
Shoaib Wani <wanishoaib@protonmail.com>
2026-01-02 11:17:31 +0530

    added environment id etc

---
+++ src/workers/scanner/collection.scanner.js
@@ -0,0 +1,53 @@
+import { Collections } from "../../models/collections.model.js";
+import RawRequest from "../../models/rawRequest.model.js";
+import RawEnvironment from "../../models/rawEnvironment.model.js";
+
+import { substituteUrlVariables, substituteNonUrlVariables } from "../../utils/variableSubstitution.js";
+
+
+export async function scanCollection({ project }) {
+    
+    const collection = await Collections.findOne({ orgId: project.orgId, collection_uid: { $in: project.collection_uids } });
+    
+    if (!collection) {
+        throw new Error("Collection not found");
+    }
+
+    
+    const requests = await RawRequest.find({ orgId: project.orgId, collectionId: collection._id }).lean();
+
+    console.log("[+] TOTAL REQUESTS: ", requests.length);
+
+    
+    // resolve env vars in the requests using the environment variables of the project
+    const environmentId = project.configuration.environment_id;
+    const environmentVariables = {};
+
+    if (environmentId) {
+        const environment = await RawEnvironment.findById(environmentId).lean();
+
+        if (environment) {
+            environment.values
+                .filter(v => v.key)
+                .forEach(v => {
+                    environmentVariables[v.key] = v.value;
+                });
+
+            console.log(`[+] Loaded ${Object.keys(environmentVariables).length} environment variables`);
+        }
+    }
+
+    const processed_requests = [];
+
+    for (let request of requests) {
+        
+        const urlResolvedRequest = substituteUrlVariables(request, environmentVariables);
+        const nonUrlResolvedRequest = substituteNonUrlVariables(urlResolvedRequest, environmentVariables);
+        
+        processed_requests.push(nonUrlResolvedRequest);
+    }
+
+    console.log("[+] TOTAL PROCESSED REQUESTS: ", processed_requests.length);
+
+    return processed_requests; 
+}
---
+++ src/workers/scanner/main.worker.js
@@ -0,0 +1,9 @@
+import { mqbroker } from "../../services/rabbitmq.service.js";
+import { scanCollection } from "./collection.scanner.js";
+
+
+
+
+export async function collectionScanOnProject(payload, msg, channel) {
+    // const { } 
+}
