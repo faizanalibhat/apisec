967c88073aa8b4cfc5e6d335ea939f3b91352ca9
Shoaib Wani <wanishoaib@protonmail.com>
2026-01-03 13:39:37 +0530

    fixed environment

--- src/models/environment.model.js
+++
@@ -1,9 +0,0 @@
-import { Schema, model } from "mongoose";
-
-
-const schema = new Schema({
-    integrationIds: { type: [Schema.Types.ObjectId], default: [] },
-}, { timestamps: true, strict: false });
-
-
-export const Environment = model("environment", schema);
