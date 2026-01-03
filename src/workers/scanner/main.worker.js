9bb590ca4789b57086cdea22c958a8fb64312f40
Shoaib Wani <wanishoaib@protonmail.com>
2026-01-03 14:26:45 +0530

    fixed scan complete in collection scan

--- src/workers/scanner/main.worker.js
+++ src/workers/scanner/main.worker.js
@@ -1,29 +1,43 @@
 import { mqbroker } from "../../services/rabbitmq.service.js";
+import Scan from "../../models/scan.model.js";
 import { getProcessedCollectionRequests } from "./collection.scanner.js";
 
-
-
-
 async function collectionScanOnProject(payload, msg, channel) {
-    const { project, scan } = payload;
+  const { project, scan } = payload;
 
-    try {
-        const requests = await getProcessedCollectionRequests({ project });
+  try {
+    const requests = await getProcessedCollectionRequests({ project });
 
-        // send requests for scan flow
-        for (let request of requests) {
-            await mqbroker.publish("apisec", "apisec.scanflow.initiate", { project, scan, request });
-        }
+    // send requests for scan flow
+    for (let request of requests) {
+      await mqbroker.publish("apisec", "apisec.scanflow.initiate", {
+        project,
+        scan,
+        request,
+      });
     }
-    catch(err) {
-        console.log(err);
-    }
-    finally {
-        channel.ack(msg);
-    }
+
+    await Scan.findOneAndUpdate(
+      { _id: scan._id },
+      { $set: { status: "completed" } },
+    );
+  } catch (err) {
+    console.log(err);
+    await Scan.findOneAndUpdate(
+      { _id: scan._id },
+      { $set: { status: "failed" } },
+    );
+  } finally {
+    channel.ack(msg);
+  }
 }
 
-
 export async function handleCollectionScan() {
-    await mqbroker.consume("apisec", "apisec.scanflow.collection", collectionScanOnProject, "collectionScanQueue", { prefetchCount: 1 });
+  await mqbroker.consume(
+    "apisec",
+    "apisec.scanflow.collection",
+    collectionScanOnProject,
+    "collectionScanQueue",
+    { prefetchCount: 1 },
+  );
 }
